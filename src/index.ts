29dbbd92b1adc40d31bfefc8b59e9df4b3a560ef
Adam Lewis <50681058+a-w-l@users.noreply.github.com>
2023-02-07 14:24:46 +0000

    updates worker response for clarity

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 export const testURL = 'https://myaccount.economist.com/id/keys';
 
-addEventListener('fetch', async (event) => {
-  const resultOfFetch = fetch(testURL, {
+const getResponseBody = async (): Promise<Response> => {
+  const result = await fetch(testURL, {
     headers: { Accept: 'application/json' },
-  });
+  }).then((r) => r.json());
 
-  console.log(resultOfFetch);
-  return event.respondWith(resultOfFetch);
+  const body = {
+    message: 'request to worker successful',
+    fetchResult: result,
+  };
+
+  return new Response(JSON.stringify(body), {
+    status: 200,
+  });
+};
+
+addEventListener('fetch', async (event) => {
+  return event.respondWith(getResponseBody());
 });
